34c4a325f8e2f19cd38a2c28b908f6525f8d8bfa
Holly Butterfield <Holly.Butterfield@clinicalink.com>
2020-09-14 09:55:22 -0400

    Product model and InMemory Product Repository built

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public String Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+        [Range(0,1000)]
+        public Decimal Price { get; set; }
+        public String Category { get; set; }
+        public String Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
